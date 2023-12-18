8f012d8487185136aa3bf9cd6b74c73f2e5d49f0
donnyandrian <116769765+donnyandrian@users.noreply.github.com>
2023-12-18 14:54:40 +0700

    init

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ function getTasks() {
     var url = 'https://github.com/donnyandrian/sti2task/blob/main/tasks.json';
     fetch(url).then((response) => response.json()).then(function(json) {
 
-    }
+    })
 }
