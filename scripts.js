e2576d27c492cb9704abdf5a1965ceb0d8325d11
donnyandrian <116769765+donnyandrian@users.noreply.github.com>
2023-12-18 16:40:03 +0700

    init

--- scripts.js
+++ scripts.js
@@ -1,9 +1,59 @@
+function makeDiv(classN) {
+    const group = document.createElement("div");
+    group.className = classN;
+
+    return group;
+}
+
+function makeText(classN, text) {
+    const item = makeDiv(classN);
+    item.innerText = text;
+
+    return item;
+}
+
 function getTasks() {
+    /*
+    <div class="container">
+        <div class="bullet"></div>
+        <div class="container_fill_1">
+            <div class="title">Title</div>
+            <div class="container_fill_2">
+                <div class="text">text</div>
+                <div class="duedate">due date</div>
+            </div>
+        </div>
+    </div>
+    */
+
     let url = 'https://raw.githubusercontent.com/donnyandrian/sti2task/main/tasks.json';
     fetch(url).then((response) => response.json()).then(function(json) {
         let parsed = JSON.parse(JSON.stringify(json));
         for (let i = 0; i < parsed.length; i++) {
             const element = parsed[i];
+
+            const content = "Mata kuliah: " + element.subject;
+
+            const title = makeText("title", element.title);
+            const text = makeText("text", content);
+            const duedate = makeText("duedate", element.dueDate);
+
+            const container_fill_1 = makeDiv("container_fill_1");
+            const container_fill_2 = makeDiv("container_fill_2");
+            
+            container_fill_2.appendChild(text);
+            container_fill_2.appendChild(duedate);
+            
+            container_fill_1.appendChild(title);
+            container_fill_1.appendChild(container_fill_2);
+
+            const container = makeDiv("container");
+            const bullet = makeDiv("bullet");
+
+            container.appendChild(bullet);
+            container.appendChild(container_fill_1);
+
+            document.getElementById("tugassec").appendChild(container);
             
             console.log(element.title);
             console.log(element.subject);
